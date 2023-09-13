fc853b5874e1dda085a27663bc51e00dfe755168
MrPingviin <foldimogyoro2000@gmail.com>
2023-09-13 15:23:44 +0200

    Comments are added + test description improvement

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const words = [
     "today",
     "I",
     "am",
-    "fine"
+    "fine",
+    "thank"
 ];
 
 // Creates a message with the given length from the given word array - without repeating words!!
@@ -20,9 +21,9 @@ const messageGenerator = (mLength, wordArray) => {
         throw new Error("The message length should be bigger or equal to the count of words in the array to avoid word repetition!");
     }
 
-    if (wordArray.length > mLength) {
+   /*  if (wordArray.length > mLength) {
         console.log("There are more words in the array than the message length so don't expect to see all of them in the message!");
-    }
+    } */
 
     if (wordArray === undefined) {
         wordArray = words;
@@ -40,4 +41,10 @@ const messageGenerator = (mLength, wordArray) => {
     return message.join(" ");
 }
 
-export { messageGenerator, words };
+const repeatChecker = (sentence) => {
+    const words = sentence.split(" ");
+    const uniqueWords = new Set(words);
+    return words.length === uniqueWords.size;
+}
+
+export { messageGenerator, words, repeatChecker };
--- index.test.js
+++ index.test.js
@@ -1,20 +1,44 @@
 import { describe } from "node:test";
-import { messageGenerator, words } from "./index.js";
+import { messageGenerator, words, repeatChecker } from "./index.js";
 import { test, expect } from "vitest";
 
-describe("messageGenerator tests with different word lengths", () => {
-    test("messageGenerator with argument of 3 should return a three word string", () => {
-        const wordLength = messageGenerator(3, words).split(" ").length;
-        expect(wordLength).toEqual(3);
-    })
-    
-    test("messageGenerator with argument of 2 should return a two word string", () => {
-        const wordLength = messageGenerator(2, words).split(" ").length;
-        expect(wordLength).toEqual(2);
-    })
+describe("Tests the lenghts of the messageGenerator return values", () => {
     
     test("messageGenerator with argument of 7 should return a seven word string", () => {
         const wordLength = messageGenerator(7, words).split(" ").length;
         expect(wordLength).toEqual(7);
+    })
+
+    test("messageGenerator with argument of 3 should return a three word string", () => {
+        const wordLength = messageGenerator(3, words).split(" ").length;
+        expect(wordLength).toEqual(3);
+    })
+
+    test("messageGenerator with argument of 13 should throw an error because the length of the given array is less (12).", () => {
+        expect(() => messageGenerator(13, words)).toThrow();
+    })
+
+    test("messageGenerator with argument of 22 should throw an error because the length of the given array is less (12).", () => {
+        expect(() => messageGenerator(22, words)).toThrow();
+    })
+
+})
+
+describe("Tests the word repetition of the messageGenerator return values", () => {
+    
+    test("repeatChecker called with messageGenerator, given a 7 as word number in the sentence and an array of words with 12 item should return true", () => {
+        expect(repeatChecker(messageGenerator(7, words))).toEqual(true);
+    })
+
+    test("repeatChecker called with messageGenerator, given a 10 as word number in the sentence and an array of words with 12 item should return true", () => {
+        expect(repeatChecker(messageGenerator(10, words))).toEqual(true);
+    })
+
+    test("repeatChecker called with messageGenerator, given a 3 as word number in the sentence and an array of words with 12 item should return true", () => {
+        expect(repeatChecker(messageGenerator(3, words))).toEqual(true);
+    })
+
+    test("repeatChecker called with messageGenerator, given a 5 as word number in the sentence and an array of words with 12 item should return true", () => {
+        expect(repeatChecker(messageGenerator(5, words))).toEqual(true);
     })
 })
